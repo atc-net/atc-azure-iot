483a5bcf4c7adf8181db38b39be66e5396ecd34b
Per Kops <per_kops@hotmail.com>
2024-04-13 12:01:13 +0200

    feat: introduce IotHubDeviceTwinGetAllCommandSettings

---
+++ src/Atc.Azure.IoT.CLI/Commands/Settings/IotHubDeviceTwinGetAllCommandSettings.cs
@@ -0,0 +1,20 @@
+namespace Atc.Azure.IoT.CLI.Commands.Settings;
+
+public class IotHubDeviceTwinGetAllCommandSettings : IotHubBaseCommandSettings
+{
+    [CommandOption("-e|--edge-devices-only <EDGE-DEVICES-ONLY>")]
+    [Description("Indicates if only edge devices should be queried.")]
+    [DefaultValue(false)]
+    public bool OnlyIncludeEdgeDevices { get; init; }
+
+    public override ValidationResult Validate()
+    {
+        var validationResult = base.Validate();
+        if (!validationResult.Successful)
+        {
+            return validationResult;
+        }
+
+        return ValidationResult.Success();
+    }
+}
