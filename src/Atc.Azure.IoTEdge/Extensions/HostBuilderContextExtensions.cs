aa64932355fd57b0054b70fe0ddbdde505621013
Per Kops <pko@delegate.dk>
2024-04-18 13:47:32 +0200

    refact: consolidate HostBuilderContextExtensions in Atc.Azure.IoTEdge library

--- src/Atc.Azure.IoTEdge/Extensions/HostBuilderContextExtensions.cs
+++ src/Atc.Azure.IoTEdge/Extensions/HostBuilderContextExtensions.cs
@@ -7,4 +7,10 @@ public static class HostBuilderContextExtensions
         => hostBuilderContext.HostingEnvironment.EnvironmentName.Equals(
             IotEdgeExecutionMode.Standalone.ToString(),
             StringComparison.Ordinal);
+
+    public static bool IsEmulatorMode(
+        this HostBuilderContext hostBuilderContext)
+        => hostBuilderContext.HostingEnvironment.EnvironmentName.Equals(
+            IotEdgeExecutionMode.Emulator.ToString(),
+            StringComparison.Ordinal);
 }
