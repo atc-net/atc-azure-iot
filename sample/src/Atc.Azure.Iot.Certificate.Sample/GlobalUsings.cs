54d30b5e4a362f81c20d5f2ab805cd24d3c8fa2e
Per Kops <pko@delegate.dk>
2024-04-23 10:37:24 +0200

    feat: add Atc.Azure.Iot.Certificate.Sample project

---
+++ sample/src/Atc.Azure.Iot.Certificate.Sample/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using System.Formats.Asn1;
+global using System.Globalization;
+global using System.Numerics;
+global using System.Security.Cryptography;
+global using System.Security.Cryptography.X509Certificates;
