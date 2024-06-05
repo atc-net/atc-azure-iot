ee251bb2f7fe32e4aefab2e4c4831803c97cf87a
Per Kops <pko@delegate.dk>
2024-06-05 13:24:10 +0200

    feat: add TenantViewModel and code to retrieve tenants

--- src/Atc.Azure.IoT.Wpf.App/MainWindowViewModel.cs
+++ src/Atc.Azure.IoT.Wpf.App/MainWindowViewModel.cs
@@ -2,6 +2,12 @@ namespace Atc.Azure.IoT.Wpf.App;
 
 public partial class MainWindowViewModel : MainWindowViewModelBase, IMainWindowViewModelBase
 {
+    private readonly AuthService authService;
+    private string authorizationText;
+    private bool isAuthorizedToAzure;
+    private bool isAuthorizedSelectedTenant;
+    private string selectedTenantId;
+
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public MainWindowViewModel()
     {
@@ -14,8 +20,49 @@ public partial class MainWindowViewModel : MainWindowViewModelBase, IMainWindowV
     {
         ArgumentNullException.ThrowIfNull(statusBarViewModel);
 
+        authService = new AuthService();
         StatusBarViewModel = statusBarViewModel;
+        AuthorizationText = "N/A";
+
+        if (IsInDesignMode)
+        {
+            Tenants.Add(new TenantViewModel { DisplayName = "heh", TenantId = Guid.NewGuid(), IsCurrent = true });
+            Tenants.Add(new TenantViewModel { DisplayName = "heh2", TenantId = Guid.NewGuid() });
+            Tenants.Add(new TenantViewModel { DisplayName = "heh3", TenantId = Guid.NewGuid() });
+        }
     }
 
     public StatusBarViewModel StatusBarViewModel { get; set; }
+
+    public ObservableCollectionEx<TenantViewModel> Tenants { get; set; } = [];
+
+    public string AuthorizationText
+    {
+        get => authorizationText;
+        set
+        {
+            authorizationText = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    public bool IsAuthorizedToAzure
+    {
+        get => isAuthorizedToAzure;
+        set
+        {
+            isAuthorizedToAzure = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    public string SelectedTenantId
+    {
+        get => selectedTenantId;
+        set
+        {
+            selectedTenantId = value;
+            RaisePropertyChanged();
+        }
+    }
 }
--- src/Atc.Azure.IoT.Wpf.App/MainWindowViewModel_Commands.cs
+++ src/Atc.Azure.IoT.Wpf.App/MainWindowViewModel_Commands.cs
@@ -4,11 +4,52 @@ public partial class MainWindowViewModel
 {
     public IRelayCommand OpenApplicationAboutCommand => new RelayCommand(OpenApplicationAboutCommandHandler);
 
+    public IRelayCommandAsync GetTenantsCommand => new RelayCommandAsync(
+        GetTenantsCommandHandler,
+        CanGetTenantsCommandHandler);
+
     private void OpenApplicationAboutCommandHandler()
     {
         // ReSharper disable once UseObjectOrCollectionInitializer
         var aboutBoxDialog = new AboutBoxDialog();
         aboutBoxDialog.IconImage.Source = App.DefaultIcon;
         aboutBoxDialog.ShowDialog();
+    }
+
+    private bool CanGetTenantsCommandHandler()
+        => !isAuthorizedToAzure;
+
+    private async Task GetTenantsCommandHandler()
+    {
+        if (!CanGetTenantsCommandHandler())
+        {
+            return;
+        }
+
+        AuthorizationText = "Authorizing...";
+
+        try
+        {
+            Tenants.Clear();
+
+
+            var data = await authService.GetTenants(CancellationToken.None).ConfigureAwait(false);
+
+            await Application.Current.Dispatcher.InvokeAsync(() =>
+            {
+                Tenants.AddRange(data);
+
+                RaisePropertyChanged(nameof(Tenants));
+
+                SelectedTenantId = Tenants.FirstOrDefault(x => x.IsCurrent)?.TenantId.ToString() ?? string.Empty;
+
+                IsAuthorizedToAzure = true;
+            });
+
+        }
+        catch (Exception ex)
+        {
+            AuthorizationText = "Authorization failed: " + ex.Message;
+        }
     }
 }
