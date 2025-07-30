453d3cd7eff3f6d710055625147f0a211a8a3f3f
Per Kops <pko@delegate.dk>
2025-07-30 11:27:04 +0200

    fix: ensure startupOrder on module contract is represented as uint instead of int

--- test/Atc.Azure.IoT.Tests/Serialization/JsonConverters/JsonModulesConverterTests.cs
+++ test/Atc.Azure.IoT.Tests/Serialization/JsonConverters/JsonModulesConverterTests.cs
@@ -42,7 +42,7 @@ public sealed class JsonModulesConverterTests
         // Assert
         Assert.NotNull(modules);
         Assert.Single(modules);
-        Assert.Equal(1, modules![0].StartupOrder);
+        Assert.Equal(1u, modules![0].StartupOrder);
     }
 
     [Fact]
@@ -72,7 +72,7 @@ public sealed class JsonModulesConverterTests
         // Assert
         Assert.NotNull(modules);
         Assert.Single(modules);
-        Assert.Equal(1, modules![0].StartupOrder);
+        Assert.Equal(1u, modules![0].StartupOrder);
     }
 
     [Fact]
@@ -102,5 +102,35 @@ public sealed class JsonModulesConverterTests
         Assert.NotNull(modules);
         Assert.Single(modules);
         Assert.Null(modules![0].StartupOrder);
+    }
+
+    [Fact]
+    public void ShouldDeserialize_WhenStartupOrder_IsMaxUInt()
+    {
+        // Arrange
+        const string json = """
+                            {
+                              "dummyModule": {
+                                "exitCode": 0,
+                                "statusDescription": "running",
+                                "runtimeStatus": "running",
+                                "lastExitTimeUtc": null,
+                                "lastStartTimeUtc": "2025-06-25T12:00:00Z",
+                                "lastRestartTimeUtc": null,
+                                "restartCount": 0,
+                                "startupOrder": 4294967295,
+                                "settings": {},
+                                "environment": null
+                              }
+                            }
+                            """;
+
+        // Act
+        var modules = JsonSerializer.Deserialize<List<Module>>(json, jsonSerializerOptions);
+
+        // Assert
+        Assert.NotNull(modules);
+        Assert.Single(modules);
+        Assert.Equal(uint.MaxValue, modules![0].StartupOrder);
     }
 }
